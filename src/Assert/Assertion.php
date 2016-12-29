277f353976f742d05d82796c6850adee40ebdfe8
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2016-12-29 02:35:30 +0100

    add choice attribute

--- src/Assert/Assertion.php
+++ src/Assert/Assertion.php
@@ -3,11 +3,48 @@
 namespace Trellis\Assert;
 
 use Assert\Assertion as BaseAssertion;
+use Trellis\EntityType\AttributeInterface;
+use Trellis\EntityTypeInterface;
 use Trellis\Error\AssertionFailed;
 
 final class Assertion extends BaseAssertion
 {
+    const MISSING_PARAM = 1000;
+
     protected static $exceptionClass = AssertionFailed::CLASS;
+
+    /**
+     * @param EntityTypeInterface|AttributeInterface $param_container
+     * @param string $param_name
+     * @param string|null $message
+     * @param string|null $property_path
+     *
+     * @return bool
+     */
+    public static function hasParam(
+        $param_container,
+        string $param_name,
+        string $message = null,
+        string $property_path = null
+    ): bool
+    {
+        if (!$param_container->hasParam($param_name)) {
+            throw static::createException($param_name, $message, static::MISSING_PARAM, $property_path);
+        }
+        return true;
+    }
+
+    public static function hasArrayParam(
+        $param_holder,
+        string $param_name,
+        string $message = null,
+        string $property_path = null
+    ): bool
+    {
+        self::hasParam($param_holder, $param_name, $message, $property_path);
+        self::isArray($param_holder->getParam($param_name), $message);
+        return true;
+    }
 
     /**
      * Make a string version of a value.
